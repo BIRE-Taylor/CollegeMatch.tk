a8a964b3f1002da591050ba2aedba2e229265555
Taylor Jones <bigfootjonesy@gmail.com>
2013-02-25 11:30:48 -0700

    Work In Progress

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1,10 +1,17 @@
-<?php
+<php
+
+include("..\models\User_model.php");
 
 class User extends Controller
 {
 	function __construct()
 	{
 		
+	}
+
+	function index()
+	{
+		echo "index";
 	}
 
 	function login()
